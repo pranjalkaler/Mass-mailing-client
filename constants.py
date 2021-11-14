16dc3c59923a69524d52a8f34d8616e533eb318e
Pranjal Kaler <pranjalkaler3041@gmail.com>
2021-11-14 19:50:11 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,23 +1,24 @@
 class Constants:
 
     # Path to the root folder
-    # EG: 'C:\\Users\\<my_username>\\Desktop\\MassMailer'
-    FILE_DIR_PATH = ""
+    # EG: 
+    FILE_DIR_PATH = 'C:\\Users\\pkaler\\Desktop\\Muskan'
 
     # Excel file where list of emails are stored. Cell A1 should contain 'Emails'
     # File should be stored in root of the project
-    EMAIL_LIST_FILE = "filename.xlsx"
+    EMAIL_LIST_FILE = "Book2.xlsx"
     TYPE_OF_FILE = ".xls"
 
     #Sender email
-    EMAIL = "yourname@gmail.com"
-    PASSWORD = "who_let_the_dogs_out?"
-    PATH_TO_PDF = "some_pdf.pdf"
-    SUBJECT = "Subject of the email"
+    EMAIL = "muskaan.rampal.spad.delhi@gmail.com"
+    PASSWORD = "QWEasd123@"
+    PATH_TO_CV = "CV_Muskaan.pdf"
+    PATH_TO_PORTFOLIO = "PORTFOLIO_MuskaanRampal.pdf"
+    SUBJECT = "Application For Internship (January 2022- June 2022)"
 
     # Populate this list with your testing emails (I used my multiple emails, 
     # as some times while testing, your sender email might get blocked)
-    TEST_EMAILS = []
+    TEST_EMAILS = ['pranjalkaler3041@gmail.com']
 
     CONTENT = """<!DOCTYPE html>
 <html>
@@ -25,6 +26,19 @@ class Constants:
     <base target="_top">
   </head>
   <body>
-    <!-- HTML formatted email body -->
+    <p>Respected Sir/Ma'am,</p>
+    <br>
+    <p>I am currently pursuing fourth year in Bachelor's of Architecture at School of Planning and Architecture, New Delhi, India. I would like to apply for a minimum 16 weeks mandatory internship (between January 2022 - June 2022) in your firm as part of the curriculum requiring professional experience.</p>
+    <p>I have been through your works quite often and I am truly inspired by it. Your firm places eminence in the architectural world. I find myself relating to your firm and aim to find a similar balance of functionality and aesthetics in my works. </p>
+    <p>Having completed more than three years in architecture I have got a solid foundation in the tools, processes, and methodologies of pursuing a career in architecture. During the course, I have taken part in various workshops and lectures. I am competent in design ideation, model making and softwares for drafting, modelling, rendering and simulations.</p>
+    <p>This internship would be a great opportunity to learn and contribute in a creative healthy environment, and thereby gain invaluable practical knowledge which will enhance my capabilities and work ethics. I would love to work with you with immense zeal. Being creative, self-motivated and hard-working, I would be a valuable addition to your team. Given the opportunity, I will surely contribute to the best of my abilities.</p>
+    <p>Portfolio and CV is attached for your perusal and necessary action.</p>
+    <p>I would be grateful if you consider me to be a part of your team.</p>
+    <p>Looking forward to hearing from you.</p>
+    <p>Portfolio at: https://issuu.com/muskaan.alka2000/docs/portfolio_b_</p>
+    <br>
+    <p>Regards,</p>
+    <p>Muskaan Rampal</p>
+    <p>School of Planning and Architecture, New Delhi</p>
   </body>
 </html>"""
